ff1e299d07d5ae2b0f06e950568bb4c1565804b7
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-08-07 14:34:17 -0300

    feat: create main pages and config router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { Room } from './pages/room'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <CreateRoom />,
+  },
+  {
+    path: '/room/:roomID',
+    element: <Room />,
+  },
+])
+
 export function App() {
-  return <h1>Teste</h1>
+  return <RouterProvider router={router} />
 }
---
+++ src/pages/create-room.tsx
@@ -0,0 +1,3 @@
+export function CreateRoom() {
+  return <h1>Create Room</h1>
+}
