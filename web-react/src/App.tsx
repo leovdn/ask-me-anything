4b59e10b1ee14c41e5fdabebdb3c1ee4becc2750
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-08-08 14:51:30 -0300

    chore: install react-query

--- web-react/src/App.tsx
+++ web-react/src/App.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
+import { queryClient } from './lib/react-query'
 
 import { CreateRoom } from './pages/create-room'
 import { Room } from './pages/room'
@@ -17,9 +19,9 @@ const router = createBrowserRouter([
 
 export function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster richColors />
-    </>
+    </QueryClientProvider>
   )
 }
