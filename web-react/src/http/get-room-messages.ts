b03fd2e5de0657d4410fe46e590dcf167e218f2a
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-08-08 16:18:31 -0300

    feat: add websocket connection to display in real-time

--- web-react/src/http/get-room-messages.ts
+++ web-react/src/http/get-room-messages.ts
@@ -2,7 +2,7 @@ interface GetRoomMessagesRequest {
   roomID: string
 }
 
-interface GetRoomMessagesResponse {
+type MessagesProps = {
   id: string
   room_id: string
   message: string
@@ -10,7 +10,18 @@ interface GetRoomMessagesResponse {
   answered: boolean
 }
 
-export async function getRoomMessages({ roomID }: GetRoomMessagesRequest) {
+export interface GetRoomMessagesResponse {
+  messages: {
+    id: string
+    text: string
+    reactionAmount: number
+    answered: boolean
+  }[]
+}
+
+export async function getRoomMessages({
+  roomID,
+}: GetRoomMessagesRequest): Promise<GetRoomMessagesResponse> {
   const response = await fetch(
     `${import.meta.env.VITE_APP_API_URL}/rooms/${roomID}/messages`,
     {
@@ -18,12 +29,12 @@ export async function getRoomMessages({ roomID }: GetRoomMessagesRequest) {
     }
   )
 
-  const data: GetRoomMessagesResponse[] = await response.json()
+  const data: MessagesProps[] = await response.json()
 
   return {
     messages: data.map((message) => ({
       id: message.id,
-      message: message.message,
+      text: message.message,
       reactionAmount: message.reaction_count,
       answered: message.answered,
     })),
